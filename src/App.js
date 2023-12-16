6c5f36d623f5836138eaccd11d5d30b5d58bee26
haesa <xdhaesa@gmail.com>
2023-12-16 17:21:58 +0900

    [Feat]: 비상 근무자 배치 프로그램 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+import { MONTH } from './constants/index.js';
+import EmergencyDutyManager from './Domain/EmergencyDutyManager.js';
+import InputView from './View/InputView.js';
+import OutputView from './View/OutputView.js';
+
 class App {
-  async run() {}
+  #month;
+  #day;
+  #sequence;
+  #workers = [];
+
+  async run() {
+    const [month, day] = await this.readMonthDay();
+    this.#month = month;
+    this.#day = day;
+    this.#sequence = await this.readWorkSequence();
+    this.positionWorker();
+    this.printTable();
+  }
+
+  async readMonthDay() {
+    try {
+      return await InputView.readMonthDay();
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      const result = await this.readMonthDay();
+      return result;
+    }
+  }
+
+  async readWorkSequence() {
+    try {
+      const weekday = await InputView.readWeekdayWorkSequence();
+      const holiday = await InputView.readHolidayWorkSequence();
+      return { weekday, holiday };
+    } catch (error) {
+      OutputView.printErrorMessage(error);
+      const result = await this.readWorkSequence();
+      return result;
+    }
+  }
+
+  positionWorker() {
+    const manager = new EmergencyDutyManager(
+      this.#month,
+      this.#day,
+      this.#sequence
+    );
+
+    for (let date = 1; date <= MONTH[this.#month]; date += 1) {
+      const worker = manager.pickWorker(this.#workers);
+      this.#workers.push(worker);
+    }
+  }
+
+  printTable() {
+    OutputView.printTable(this.#workers);
+  }
 }
 
 export default App;
