a953ae6bbecfcc3ac3b7cda7843f51ad50eb85b4
haesa <xdhaesa@gmail.com>
2023-12-16 17:19:58 +0900

    [Fix]: InputView 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,16 +1,23 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE } from '../constants/index.js';
+import InputValidator from '../Validator/InputValidator.js';
 
 const InputView = {
   async readMonthDay() {
     const input = await Console.readLineAsync(INPUT_MESSAGE.monthDay);
     InputValidator.monthDay(input);
-    const [month, day] = input.split(',').trim((value) => value.trim());
+    const [month, day] = input.split(',').map((value) => value.trim());
     return [Number(month), day];
   },
 
-  async readWorkSequence(message) {
-    const input = await Console.readLineAsync(message);
+  async readWeekdayWorkSequence() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.weekday);
+    InputValidator.workSequence(input);
+    return input.split(',').map((value) => value.trim());
+  },
+
+  async readHolidayWorkSequence() {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.holiday);
     InputValidator.workSequence(input);
     return input.split(',').map((value) => value.trim());
   },
